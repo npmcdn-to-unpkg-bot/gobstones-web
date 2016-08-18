ddb21ea6620328b611e77e56c861d6882fba278a
Rodrigo Alfonso <rodri042@gmail.com>
2016-08-18 04:07:13 -0300

    Update 2016-08-18T07:07:10.248Z

--- scripts/stylist.js
+++ scripts/stylist.js
@@ -27,7 +27,7 @@ var Stylist = function () {
           $(panelCssClass + " .ui-resizable-s").hide();
           $(panelCssClass + " .ui-resizable-se").hide();
 
-          _this.scaleBoardAndPutInCenter(_this.DEFAULT_PERCENTAGE, boardCssClass);
+          _this.scaleAndCenterBoard(_this.DEFAULT_PERCENTAGE, boardCssClass);
         }, 0);
       });
 
@@ -39,34 +39,40 @@ var Stylist = function () {
       var _this2 = this;
 
       $(window).resize(function () {
-        // keep aspect ratio on window resize:
-        var documentWidth = $(document).width();
-        if (!_this2.lastSize) {
-          _this2.lastSize = documentWidth;
-          return;
-        }
-
-        var leftPanel = $(panelCssClass);
-        var percentage = leftPanel.width() / _this2.lastSize;
-
-        leftPanel.width(percentage * documentWidth);
-        _this2.lastSize = documentWidth;
-
-        // adapt board size to panel:
-        _this2.scaleBoardAndPutInCenter(percentage, boardCssClass);
+        var percentage = _this2.keepAspectRatioOnWindowResize(panelCssClass);
+        _this2.scaleAndCenterBoard(percentage, boardCssClass);
       });
     }
   }, {
-    key: "scaleBoardAndPutInCenter",
-    value: function scaleBoardAndPutInCenter(percentage, boardCssClass) {
-      if (!this.originalHeight) this.originalHeight = $(boardCssClass).height();
+    key: "keepAspectRatioOnWindowResize",
+    value: function keepAspectRatioOnWindowResize(panelCssClass) {
+      var documentWidth = $(document).width();
+      if (!this.lastSize) {
+        this.lastSize = documentWidth;
+        return this.DEFAULT_PERCENTAGE;
+      }
 
+      var leftPanel = $(panelCssClass);
+      var percentage = leftPanel.width() / this.lastSize;
+
+      leftPanel.width(percentage * documentWidth);
+      this.lastSize = documentWidth;
+      return percentage;
+    }
+  }, {
+    key: "scaleAndCenterBoard",
+    value: function scaleAndCenterBoard(percentage, boardCssClass) {
       var scaleDiff = -(percentage / this.DEFAULT_PERCENTAGE) + 1;
       var scale = this.INITIAL_SCALE + scaleDiff;
       $(boardCssClass).css("transform", "scale(" + scale + ")");
-
+      this.centerBoardVertically(scale, boardCssClass);
+    }
+  }, {
+    key: "centerBoardVertically",
+    value: function centerBoardVertically(scale, boardCssClass) {
+      var originalHeight = $(boardCssClass).height() / scale;
       var middleY = ($(document).height() - this.TOOLBAR_HEIGHT) / 2;
-      var offsetY = this.originalHeight / 2;
+      var offsetY = originalHeight / 2;
       $(boardCssClass).css("margin-top", middleY - offsetY + "px");
     }
   }]);
